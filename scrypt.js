aa5fc124f068d460ff76da3b274f91ce532a5be4
giogua03 <giorgiogua03@gmail.com>
2022-10-28 14:27:30 +0200

    fine

--- scrypt.js
+++ scrypt.js
@@ -1,16 +1,16 @@
-const arrGroceryList = ['cotone', 'marmellata'];
-const eleButton = document.getElementById('.button');
-const inputGroceryItem = document.getElementById('.inserisci');
-const eleGroceryList = document.getElementById('.grocery-list');
+const arrGroceryList = ['cotone', 'marmellata', 'frutta'];
+const eleButton = document.getElementById('button');
+const inputGroceryItem = document.getElementById('inserisci');
+const eleGroceryList = document.getElementById('grocery-list');
 
-
-
-
-eleButton.addEventListener('click', function() {
 let i = 0
 while (i < arrGroceryList.length){
     eleGroceryList.innerHTML += `<li>${arrGroceryList[i]}</li>`;
     i++;
 
 }
-}
+
+
+eleButton.addEventListener('click', function() {
+
+})
